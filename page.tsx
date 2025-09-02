417a577c8bc040899edc7513b8049a43fb2dda24
Javenshawn <163969778+Javenshawn@users.noreply.github.com>
2025-09-02 17:20:29 +0700

    Delete page.tsx

--- page.tsx
+++
@@ -1,8 +0,0 @@
-export default function CancelPage() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-8">
-      <h1 className="text-4xl font-bold mb-4">Payment Cancelled</h1>
-      <p>If you change your mind, you can try again at any time.</p>
-    </div>
-  );
-}
